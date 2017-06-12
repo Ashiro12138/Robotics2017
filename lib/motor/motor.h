6d7442a63900f2faed888192b0ad8693f92a30be
Jason Wang <24618@bbc.qld.edu.au>
2017-06-12 14:53:20 +1000

    Now it's actually working

--- lib/motor/motor.h
+++ lib/motor/motor.h
@@ -1,13 +1,13 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 class Motor{
 public:
   int enAPin, in1Pin, in2Pin;
   void Setup(int enA, int in1, int in2);
-  void Move(int speed, int direction);  
+  void Move(int speed, int direction);
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
-#include "Arduino.h"
-#include <motor.h>
-#include <cstdio>
+#include <Arduino.h>
+#include "motor.h"
 
 int main(){
 	Motor motor1;
-	motor1.setup(10, 9, 8);
-	motor1.move(255, 1);
+	motor1.Setup(10, 9, 8);
+	motor1.Move(255, 1);
 	return 0;
 }
